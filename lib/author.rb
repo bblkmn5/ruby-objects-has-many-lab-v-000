6f8daec38900da7a9ab8c4706c7b34c483e7f868
Ben Blackman <bblkmn5@gmail.com>
2017-10-19 04:49:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,8 +19,11 @@ attr_accessor :name, :post
   end
 
   def add_post_by_title(post)
-    
-    
+    post = Post.new(name)
+    self.posts << post
+    post.author = self
+    @@post_count +=1
+
 
 
 end
